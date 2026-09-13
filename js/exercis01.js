ad69a178e3fcdb377f677205f2ba414fc98a2991
Stanislav <ssovelich@gmail.com>
2026-09-13 14:22:18 +0300

    added exercise 12

---
+++ js/exercis01.js
@@ -0,0 +1,79 @@
+"use strict";
+
+//** task 1 */
+
+const message1 = 'Hello';
+const message2 = 'JavaScript';
+
+alert(message1 + ' ' + message2);
+
+//** task 2 */
+
+// const x = Number(prompt('Введіть значення x:'));
+// const y = Number(prompt('Введіть значення y:'));
+
+// const sum = x + y;
+
+// alert(sum);
+
+//** task 3 */
+
+// const secret_number = 4;
+
+// const user_number = Number(prompt("Вгадайте число від 1 до 10:"));
+
+// if (user_number === secret_number) {
+//   alert("Congratulations, You did it!");
+// } else if (user_number > secret_number) {
+//   alert("The number is too long");
+// } else {
+//   alert("The number is short");
+// }
+
+//** task 4 */
+
+// const day = prompt("Введіть день тижня (наприклад, Понеділок або Monday):");
+
+// if (day === "Понеділок" || day === "Monday") {
+//   alert("Start of the work week!");
+// } else if (day === "П'ятниця" || day === "Friday") {
+//   alert("End of the work week!");
+// } else {
+//   alert("A regular day");
+// }
+
+// switch (day) {
+//   case "Понеділок":
+//   case "Monday":
+//     alert("Start of the work week!");
+//     break;
+
+//   case "П'ятниця":
+//   case "Friday":
+//     alert("End of the work week!");
+//     break;
+
+//   default:
+//     alert("A regular day");
+//     break;
+// }
+
+//** task 5 */
+
+// const score = Number(prompt("Введіть кількість балів:"));
+
+// let grade;
+
+// if (score < 50) {
+//   grade = "F";
+// } else if (score < 70) {
+//   grade = "D";
+// } else if (score < 80) {
+//   grade = "C";
+// }   else if (score < 90) {
+//   grade = "B";
+// } else {
+//   grade = "A";
+// }
+
+// alert(grade);
